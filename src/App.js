55e26324b6cf414338c974a16cb1e378d8d4fba2
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-11 20:51:43 +0100

    add-api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import "./style/style.css";
 
 function App() {
+  const API_KEY = "280d907cafe3e263b96e3b945a9562cc";
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    search("LONDON");
+  }, []);
+
   return (
     <div className="App">
       <div className="weather-widget">
